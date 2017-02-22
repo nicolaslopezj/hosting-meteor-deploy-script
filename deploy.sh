fba6086799c73ac2fbfae91c143e7e85502617cc
Nicolás López <nicolaslopezj@me.com>
2017-02-21 23:50:27 -0300

    update to deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,23 @@
-pwd
-echo "root url"
-echo $ROOT_URL
-echo "settings"
-echo $METEOR_SETTINGS
+# Install things
+sudo apt-get update
+sudo apt-get install build-essential -y
+sudo apt-get install python2.7 -y
+export PYTHON="/usr/bin/python2.7"
+
+# Install Meteor
+curl https://install.meteor.com/ | sh
+
+# Go to app folder
+cd app
+cd $APP_LOCATION # Add an if here
+
+# Install NPM Deps
+meteor npm install
+
+# Build app
+meteor build --directory ~/compiled --server-only
+
+# Go to compiled app
+cd ~/compiled/bundle/programs/server
+# Install production modules
+meteor npm install --production
---
+++ start.sh
@@ -0,0 +1,4 @@
+# Go to compiled app
+cd ~/compiled/bundle
+# Run app
+sudo -E meteor node main.js
