6e6839c9730b1a690b66e56c29c690a96dcd2efb
Nicolás López <nicolaslopezj@me.com>
2017-02-28 08:15:40 -0300

    add new line to pipe log

--- pipeLogs.sh
+++ pipeLogs.sh
@@ -1,5 +1,6 @@
 # This script passes the meteor app logs and puts then in the orion.hosting app.log file
 while true; do
   sudo docker logs meteor --since 2s >> ~/app.log
+  echo "\n" >> ~/app.log
   sleep 2s
 done
