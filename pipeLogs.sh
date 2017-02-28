9149e854d9b947326d206396b1e3ec2ab1c9fb99
Nicolás López <nicolaslopezj@me.com>
2017-02-28 07:47:29 -0300

    pipe per 2 seconds

--- pipeLogs.sh
+++ pipeLogs.sh
@@ -1,5 +1,5 @@
 # This script passes the meteor app logs and puts then in the orion.hosting app.log file
 while true; do
-  sudo docker logs meteor --since 5s >> ~/app.log
-  sleep 5
+  sudo docker logs meteor --since 2s >> ~/app.log
+  sleep 2s
 done
