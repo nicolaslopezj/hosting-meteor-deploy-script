0224ba32a3d31e9fe7fe80a002633b9ded434443
Nicolás López <nicolaslopezj@me.com>
2017-02-28 06:36:39 -0300

    use double cat

--- pipeLogs.sh
+++ pipeLogs.sh
@@ -1,5 +1,5 @@
 # This script passes the meteor app logs and puts then in the orion.hosting app.log file
 while true; do
-  sudo docker logs meteor --since 5s > ~/app.log
+  sudo docker logs meteor --since 5s >> ~/app.log
   sleep 5
 done
