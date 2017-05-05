a81edad08fed39f4aea6877c37194c7f82017854
Nicolás López <nicolaslopezj@me.com>
2017-05-05 16:35:57 -0300

    fix script

--- check.sh
+++ check.sh
@@ -7,10 +7,10 @@ elaspsed=0
 while [[ true ]]; do
   sleep 1
   elaspsed=$((elaspsed+1))
-  curl --fail -L localhost:$DEPLOY_CHECK_PORT && sudo docker logs meteor --tail=300 && exit 0
+  curl --fail -L localhost:$DEPLOY_CHECK_PORT && exit 0
 
   if [ "$elaspsed" == "$DEPLOY_CHECK_WAIT_TIME" ]; then
-    revert_app
+    sudo docker logs meteor --since 80s
     exit 1
   fi
 done
