c7006c86784ca9ed3652464ef849d48bd299b415
Nicolás López Jullian <nicolaslopezj@me.com>
2017-05-08 17:49:12 -0300

    Update check.sh

--- check.sh
+++ check.sh
@@ -4,7 +4,7 @@ set -e
 echo "Starting check deploy"
 
 DEPLOY_CHECK_PORT=80
-DEPLOY_CHECK_WAIT_TIME=13
+DEPLOY_CHECK_WAIT_TRIES=20
 elaspsed=1
 
 while true; do
@@ -13,7 +13,7 @@ while true; do
   elaspsed=$((elaspsed+1))
   curl --fail -L localhost:$DEPLOY_CHECK_PORT && exit 0
 
-  if [ $elaspsed -ge $DEPLOY_CHECK_WAIT_TIME ]; then
+  if [ $elaspsed -ge $DEPLOY_CHECK_WAIT_TRIES ]; then
     echo "Error starting app"
     sudo docker logs meteor
     exit 1
