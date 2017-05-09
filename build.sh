365f48dbea558f6303c3b5c4754311ea8da5187e
Nicolás López Jullian <nicolaslopezj@me.com>
2017-05-09 17:11:55 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ echo "====> Installing app npm dependencies..."
 echo ""
 export PYTHON="/usr/bin/python2.7"
 meteor npm install > /dev/null
-meteor add npm-bcrypt@0.8.7
+npm install --save bcryptjs && npm uninstall --save bcrypt
 
 # Build app
 # export METEOR_PROFILE=100
