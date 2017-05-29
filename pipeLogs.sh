eb8d67a08ff37cb8b6a8bb46a52dc52786a3bc41
Nicolás López Jullian <nicolaslopezj@me.com>
2017-05-29 13:44:43 -0400

    Update pipeLogs.sh

--- pipeLogs.sh
+++ pipeLogs.sh
@@ -1,6 +1,6 @@
 # This script passes the meteor app logs and puts then in the orion.hosting app.log file
 while true; do
-  sudo docker logs meteor --since 2s >> /home/deploy/app.log
+  ((sudo docker logs meteor --since 2s) 2>&1) >> /home/deploy/app.log
   echo "\n" >> /home/deploy/app.log
   sleep 2s
 done
