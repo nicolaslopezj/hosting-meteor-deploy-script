6e384705909e06e86ec1b0abce6b6c7d3217e88d
Nicolás López Jullian <nicolaslopezj@me.com>
2017-10-31 19:17:57 -0300

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -27,7 +27,7 @@ echo "====> Installing Yarn..."
 echo ""
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt-get update && sudo apt-get install yarn
+sudo apt-get update && sudo apt-get install yarn -y
 
 # Install docker
 echo ""
