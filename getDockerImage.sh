850a7459fe798d314a7eb8eb86374d0383516cd6
Nicolás López <nicolaslopezj@me.com>
2017-11-02 10:53:17 -0300

    update v

--- getDockerImage.sh
+++ getDockerImage.sh
@@ -7,7 +7,7 @@ pwd
 ls
 
 # Go to app folder
-cd /home/deploy/code
+cd /home/deploy/app
 pwd
 ls
 if [[ "$APP_LOCATION" != "" ]]; then
@@ -25,18 +25,22 @@ METEOR_DOCKER_1_4="abernix/meteord:base"
 FINAL_DOCKER_IMAGE=$METEOR_DOCKER_1_6
 
 if [[ "$METEOR_VERSION" == "METEOR@1.6"* ]]; then
+  echo "found meteor 1.6"
   FINAL_DOCKER_IMAGE=$METEOR_DOCKER_1_6
 fi
 
 if [[ "$METEOR_VERSION" == "METEOR@1.5"* ]]; then
+  echo "found meteor 1.5"
   FINAL_DOCKER_IMAGE=$METEOR_DOCKER_1_5
 fi
 
 if [[ "$METEOR_VERSION" == "METEOR@1.4"* ]]; then
+  echo "found meteor 1.4"
   FINAL_DOCKER_IMAGE=$METEOR_DOCKER_1_4
 fi
 
 if [[ "$DOCKER_IMAGE" != "" ]]; then
+  echo "meteor version not found"
   FINAL_DOCKER_IMAGE=$DOCKER_IMAGE
 fi
 
