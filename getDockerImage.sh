07bd86359bd2dbc2dec53998b1b5b9403528f877
Nicolás López <nicolaslopezj@me.com>
2017-11-02 16:31:23 -0300

    test

--- getDockerImage.sh
+++ getDockerImage.sh
@@ -4,11 +4,14 @@
 
 # Go to app folder
 cd /home/deploy/app
-if [[ "$APP_LOCATION" != "" ]]; then
+# cd $APP_LOCATION
+
+if [ "$APP_LOCATION" != "" ]; then
   cd $APP_LOCATION
 fi
 
 METEOR_VERSION=$(cat ./.meteor/release)
+# METEOR_VERSION="METEOR@1.5"
 
 METEOR_DOCKER_1_6="mrauhu/meteord:node-8.4.0-base"
 METEOR_DOCKER_1_5="abernix/meteord:base"
@@ -16,19 +19,19 @@ METEOR_DOCKER_1_4="abernix/meteord:base"
 
 FINAL_DOCKER_IMAGE=$METEOR_DOCKER_1_6
 
-if [[ "$METEOR_VERSION" == "METEOR@1.6"* ]]; then
+if [ "$METEOR_VERSION" == "METEOR@1.6"* ]; then
   FINAL_DOCKER_IMAGE=$METEOR_DOCKER_1_6
 fi
 
-if [[ "$METEOR_VERSION" == "METEOR@1.5"* ]]; then
+if [ "$METEOR_VERSION" == "METEOR@1.5"* ]; then
   FINAL_DOCKER_IMAGE=$METEOR_DOCKER_1_5
 fi
 
-if [[ "$METEOR_VERSION" == "METEOR@1.4"* ]]; then
+if [ "$METEOR_VERSION" == "METEOR@1.4"* ]; then
   FINAL_DOCKER_IMAGE=$METEOR_DOCKER_1_4
 fi
 
-if [[ "$DOCKER_IMAGE" != "" ]]; then
+if [ "$DOCKER_IMAGE" != "" ]; then
   FINAL_DOCKER_IMAGE=$DOCKER_IMAGE
 fi
 
