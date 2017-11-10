a7c688df7b437df540f291cb6a97e687fc861ca5
Nicolás López <nicolaslopezj@me.com>
2017-11-10 15:39:29 -0300

    change check.sh

--- check.sh
+++ check.sh
@@ -4,14 +4,14 @@ set -e
 echo "Starting check deploy"
 
 DEPLOY_CHECK_PORT=80
-DEPLOY_CHECK_WAIT_TRIES=45
+DEPLOY_CHECK_WAIT_TRIES=30
 elaspsed=1
 
 while true; do
   echo "Check deploy #$elaspsed..."
   sleep 5
   elaspsed=$((elaspsed+1))
-  curl --fail --head --insecure localhost:$DEPLOY_CHECK_PORT && exit 0
+  curl --fail --head --insecure --max-time 1 "localhost:$DEPLOY_CHECK_PORT" && exit 0
 
   if [ $elaspsed -ge $DEPLOY_CHECK_WAIT_TRIES ]; then
     echo "Error starting app"
